09a8e5b04fafb6f8b4ccf3876ede7f9af1fd5be8
nihaltowfiq <nihaltowfiq@gmail.com>
2021-01-18 16:04:24 +0600

    added validation

--- js/script.js
+++ js/script.js
@@ -10,16 +10,18 @@ searchBtn.addEventListener("click", (e) => {
     fetch(`https://api.github.com/users/${userText}`)
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
         if (data.message == "Not Found") {
           //show alert
+          ui.showAlert("User not Found!", "alert alert-danger");
         } else {
           // show profile
           ui.showProfile(data);
+          // ui.showAlert("User Found!", "alert alert-success");
         }
       });
   } else {
     // clear profile
+    ui.clearProfile();
   }
   e.preventDefault();
 });
--- js/ui.js
+++ js/ui.js
@@ -3,11 +3,12 @@ class UI {
     this.profile = document.querySelector("#profile");
   }
   showProfile(user) {
+    this.clearAlert();
     this.profile.innerHTML = `
         <div class="card card-body mb-3">
             <div class="row">
                 <div class="col-md-3">
-                    <img class="img-fluid mb-2" src="${user.avatar_url}">
+                    <img class="img-fluid mb-2 rounded" src="${user.avatar_url}">
                     <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
                 </div>
 
@@ -19,13 +20,40 @@ class UI {
                     <br><br>
                     <ul class="list-group">
                         <li class="list-group-item">Company: ${user.company}</li>
-                        <li class="list-group-item">Website/Blog: ${user.blog}</li>
+                        <li class="list-group-item">Website/Blog: <a href="${user.blog}" target="_blank" >${user.blog}</a></li>
                         <li class="list-group-item">Location: ${user.location}</li>
                         <li class="list-group-item">Member Since: ${user.created_at}</li>
-                    </ul>
+                    </ul>                    
                 </div>
             </div>
         </div>
         `;
+  }
+
+  clearProfile() {
+    this.profile.innerHTML = "";
+  }
+
+  showAlert(message, className) {
+    this.clearAlert();
+    this.clearProfile();
+    let div = document.createElement("div");
+    div.className = `text-center ${className}`;
+    div.appendChild(document.createTextNode(message));
+
+    const container = document.querySelector(".searchContainer");
+    const search = document.querySelector(".search");
+    container.insertBefore(div, search);
+
+    setTimeout(() => {
+      div.remove();
+    }, 3000);
+  }
+
+  clearAlert() {
+    const currentAlert = document.querySelector(".alert");
+    if (currentAlert) {
+      currentAlert.remove();
+    }
   }
 }
